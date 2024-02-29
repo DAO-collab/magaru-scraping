0c64c2c2efa7f6407aef471750e80dc460b7fc4a
morning120429 <forever120429@gmail.com>
2024-02-29 18:10:49 +1000

    change file

--- Company data/number.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open('total(sch-1-4, pci-1-4).json', 'r') as file:
-    data = json.load(file)
-
-print(len(data))
--- Company data/remove_duplicate.py
+++ Company data/remove_duplicate.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Read the Excel file
-df = pd.read_excel("SCH(0-26692).xlsx")
+df = pd.read_excel("sample.xlsx")
 
 # Remove duplicates based on all columns
 df = df.drop_duplicates()
 
 # Save the updated DataFrame to a new Excel file
-df.to_excel("a.xlsx", index=False)
+df.to_excel("output.xlsx", index = False)
